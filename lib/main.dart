44da311e17e5bc9052dfe8fadcbe7e8d5e504305
undreameba <maunderabe@gmail.com>
2025-10-17 09:05:51 +0100

    implement dynamic text in OrderItemDisplay

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class OrderItemDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('This is a placeholder for OrderItemDisplay');
+    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
   }
 }
 
